c942aa4487ed95227d755aa52c87666265c2c68c
vindian <vishire@gmail.com>
2022-08-30 09:55:04 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
 <H1>Version A </H1>
-<H3> Hellow World </H1>
+<H3> Hello - Abu Dhabi and Dubai, Demo on 30th August </H1>
 <img src="a-abtesting.png">
 <div>
 <?php 
