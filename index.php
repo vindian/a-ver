3c462837557076da06f382446d91b4c93079d989
vindian <vishire@gmail.com>
2024-04-30 13:06:24 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
 <H1>Version A </H1>
-<H3> Hello from OpenShift Application Demo - October 2023  </H1>
+<H3> Hello from OpenShift Application Demo - April 2024  </H1>
 <img src="a-abtesting.png">
 <div>
 <?php 
